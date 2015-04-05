2fa4f00bc630d0e189c843f7871aa4196d733540
mchengal <mchengal@users.noreply.github.com>
2015-04-05 10:49:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.configure(function () {
     app.use(express.bodyParser());
     app.use(express.methodOverride());
     app.use(app.router);
-    app.use(express.static(__dirname + '/public_stage_3'));
+    app.use(express.static(__dirname + '/public'));
 });
 
 
